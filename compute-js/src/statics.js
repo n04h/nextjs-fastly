31b6597762667abac1fa64a05134529801d90055
n04h <contact@n04h.jp>
2022-10-07 12:43:59 +0000

    feat: Compute@Edgeに必要なファイル生成

---
+++ compute-js/src/statics.js
@@ -0,0 +1,151 @@
+import { Buffer } from "buffer";
+import file0 from "../../.next/BUILD_ID?staticBinary";
+import file1 from "../../.next/build-manifest.json?staticText";
+import file2 from "../../.next/export-marker.json?staticText";
+import file3 from "../../.next/images-manifest.json?staticText";
+import file4 from "../../.next/next-server.js.nft.json?staticText";
+import file5 from "../../.next/package.json?staticText";
+import file6 from "../../.next/prerender-manifest.json?staticText";
+import file7 from "../../.next/react-loadable-manifest.json?staticText";
+import file8 from "../../.next/required-server-files.json?staticText";
+import file9 from "../../.next/routes-manifest.json?staticText";
+import file10 from "../../.next/server/chunks/675.js?staticText";
+import * as fileModule10 from "../../.next/server/chunks/675.js";
+import file11 from "../../.next/server/chunks/font-manifest.json?staticText";
+import * as fileModule11 from "../../.next/server/chunks/font-manifest.json";
+import file12 from "../../.next/server/font-manifest.json?staticText";
+import * as fileModule12 from "../../.next/server/font-manifest.json";
+import file13 from "../../.next/server/middleware-build-manifest.js?staticText";
+import * as fileModule13 from "../../.next/server/middleware-build-manifest.js";
+import file14 from "../../.next/server/middleware-manifest.json?staticText";
+import * as fileModule14 from "../../.next/server/middleware-manifest.json";
+import file15 from "../../.next/server/middleware-react-loadable-manifest.js?staticText";
+import * as fileModule15 from "../../.next/server/middleware-react-loadable-manifest.js";
+import file16 from "../../.next/server/pages/404.html?staticText";
+import file17 from "../../.next/server/pages/500.html?staticText";
+import file18 from "../../.next/server/pages/_app.js?staticText";
+import * as fileModule18 from "../../.next/server/pages/_app.js";
+import file19 from "../../.next/server/pages/_app.js.nft.json?staticText";
+import * as fileModule19 from "../../.next/server/pages/_app.js.nft.json";
+import file20 from "../../.next/server/pages/_document.js?staticText";
+import * as fileModule20 from "../../.next/server/pages/_document.js";
+import file21 from "../../.next/server/pages/_document.js.nft.json?staticText";
+import * as fileModule21 from "../../.next/server/pages/_document.js.nft.json";
+import file22 from "../../.next/server/pages/_error.js?staticText";
+import * as fileModule22 from "../../.next/server/pages/_error.js";
+import file23 from "../../.next/server/pages/_error.js.nft.json?staticText";
+import * as fileModule23 from "../../.next/server/pages/_error.js.nft.json";
+import file24 from "../../.next/server/pages/api/hello.js?staticText";
+import * as fileModule24 from "../../.next/server/pages/api/hello.js";
+import file25 from "../../.next/server/pages/api/hello.js.nft.json?staticText";
+import * as fileModule25 from "../../.next/server/pages/api/hello.js.nft.json";
+import file26 from "../../.next/server/pages/api/user/[name].js?staticText";
+import * as fileModule26 from "../../.next/server/pages/api/user/[name].js";
+import file27 from "../../.next/server/pages/api/user/[name].js.nft.json?staticText";
+import * as fileModule27 from "../../.next/server/pages/api/user/[name].js.nft.json";
+import file28 from "../../.next/server/pages/index.html?staticText";
+import file29 from "../../.next/server/pages/index.js.nft.json?staticText";
+import * as fileModule29 from "../../.next/server/pages/index.js.nft.json";
+import file30 from "../../.next/server/pages-manifest.json?staticText";
+import * as fileModule30 from "../../.next/server/pages-manifest.json";
+import file31 from "../../.next/server/webpack-api-runtime.js?staticText";
+import * as fileModule31 from "../../.next/server/webpack-api-runtime.js";
+import file32 from "../../.next/server/webpack-runtime.js?staticText";
+import * as fileModule32 from "../../.next/server/webpack-runtime.js";
+import file33 from "../../.next/static/BfVG9zrgsk-ihswGhBw49/_buildManifest.js?staticText";
+import file34 from "../../.next/static/BfVG9zrgsk-ihswGhBw49/_ssgManifest.js?staticText";
+import file35 from "../../.next/static/chunks/framework-7dc8a65f4a0cda33.js?staticText";
+import file36 from "../../.next/static/chunks/main-a5af4d1b6edd23b1.js?staticText";
+import file37 from "../../.next/static/chunks/pages/_app-e64cee366c168257.js?staticText";
+import file38 from "../../.next/static/chunks/pages/_error-a4ba2246ff8fb532.js?staticText";
+import file39 from "../../.next/static/chunks/pages/index-cffb9a60bb2b6465.js?staticText";
+import file40 from "../../.next/static/chunks/polyfills-c67a75d1b6f99dc8.js?staticText";
+import file41 from "../../.next/static/chunks/webpack-69bfa6990bb9e155.js?staticText";
+import file42 from "../../.next/static/css/27d177a30947857b.css?staticText";
+import file43 from "../../.next/static/css/adf6895ec356eb64.css?staticText";
+import file44 from "../../.next/trace?staticBinary";
+import file45 from "../../README.md?staticBinary";
+import file46 from "../../next-env.d.ts?staticBinary";
+import file47 from "../../next.config.js?staticText";
+import * as fileModule47 from "../../next.config.js";
+import file48 from "../../package-lock.json?staticText";
+import file49 from "../../package.json?staticText";
+import file50 from "../../src/pages/_app.tsx?staticBinary";
+import file51 from "../../src/pages/api/hello.ts?staticBinary";
+import file52 from "../../src/pages/api/user/[name].ts?staticBinary";
+import file53 from "../../src/pages/index.tsx?staticBinary";
+import file54 from "../../src/public/favicon.ico?staticBinary";
+import file55 from "../../src/public/vercel.svg?staticText";
+import file56 from "../../src/styles/Home.module.css?staticText";
+import file57 from "../../src/styles/globals.css?staticText";
+import file58 from "../../tsconfig.json?staticText";
+
+export const assets = {
+  "/.next/BUILD_ID": { contentType: undefined, content: Buffer.from(file0, "base64"), module: null, isStatic: false },
+  "/.next/build-manifest.json": { contentType: "application/json", content: file1, module: null, isStatic: false },
+  "/.next/export-marker.json": { contentType: "application/json", content: file2, module: null, isStatic: false },
+  "/.next/images-manifest.json": { contentType: "application/json", content: file3, module: null, isStatic: false },
+  "/.next/next-server.js.nft.json": { contentType: "application/json", content: file4, module: null, isStatic: false },
+  "/.next/package.json": { contentType: "application/json", content: file5, module: null, isStatic: false },
+  "/.next/prerender-manifest.json": { contentType: "application/json", content: file6, module: null, isStatic: false },
+  "/.next/react-loadable-manifest.json": { contentType: "application/json", content: file7, module: null, isStatic: false },
+  "/.next/required-server-files.json": { contentType: "application/json", content: file8, module: null, isStatic: false },
+  "/.next/routes-manifest.json": { contentType: "application/json", content: file9, module: null, isStatic: false },
+  "/.next/server/chunks/675.js": { contentType: "application/javascript", content: file10, module: fileModule10, isStatic: false },
+  "/.next/server/chunks/font-manifest.json": { contentType: "application/json", content: file11, module: fileModule11, isStatic: false },
+  "/.next/server/font-manifest.json": { contentType: "application/json", content: file12, module: fileModule12, isStatic: false },
+  "/.next/server/middleware-build-manifest.js": { contentType: "application/javascript", content: file13, module: fileModule13, isStatic: false },
+  "/.next/server/middleware-manifest.json": { contentType: "application/json", content: file14, module: fileModule14, isStatic: false },
+  "/.next/server/middleware-react-loadable-manifest.js": { contentType: "application/javascript", content: file15, module: fileModule15, isStatic: false },
+  "/.next/server/pages/404.html": { contentType: "text/html", content: file16, module: null, isStatic: false },
+  "/.next/server/pages/500.html": { contentType: "text/html", content: file17, module: null, isStatic: false },
+  "/.next/server/pages/_app.js": { contentType: "application/javascript", content: file18, module: fileModule18, isStatic: false },
+  "/.next/server/pages/_app.js.nft.json": { contentType: "application/json", content: file19, module: fileModule19, isStatic: false },
+  "/.next/server/pages/_document.js": { contentType: "application/javascript", content: file20, module: fileModule20, isStatic: false },
+  "/.next/server/pages/_document.js.nft.json": { contentType: "application/json", content: file21, module: fileModule21, isStatic: false },
+  "/.next/server/pages/_error.js": { contentType: "application/javascript", content: file22, module: fileModule22, isStatic: false },
+  "/.next/server/pages/_error.js.nft.json": { contentType: "application/json", content: file23, module: fileModule23, isStatic: false },
+  "/.next/server/pages/api/hello.js": { contentType: "application/javascript", content: file24, module: fileModule24, isStatic: false },
+  "/.next/server/pages/api/hello.js.nft.json": { contentType: "application/json", content: file25, module: fileModule25, isStatic: false },
+  "/.next/server/pages/api/user/[name].js": { contentType: "application/javascript", content: file26, module: fileModule26, isStatic: false },
+  "/.next/server/pages/api/user/[name].js.nft.json": { contentType: "application/json", content: file27, module: fileModule27, isStatic: false },
+  "/.next/server/pages/index.html": { contentType: "text/html", content: file28, module: null, isStatic: false },
+  "/.next/server/pages/index.js.nft.json": { contentType: "application/json", content: file29, module: fileModule29, isStatic: false },
+  "/.next/server/pages-manifest.json": { contentType: "application/json", content: file30, module: fileModule30, isStatic: false },
+  "/.next/server/webpack-api-runtime.js": { contentType: "application/javascript", content: file31, module: fileModule31, isStatic: false },
+  "/.next/server/webpack-runtime.js": { contentType: "application/javascript", content: file32, module: fileModule32, isStatic: false },
+  "/.next/static/BfVG9zrgsk-ihswGhBw49/_buildManifest.js": { contentType: "application/javascript", content: file33, module: null, isStatic: false },
+  "/.next/static/BfVG9zrgsk-ihswGhBw49/_ssgManifest.js": { contentType: "application/javascript", content: file34, module: null, isStatic: false },
+  "/.next/static/chunks/framework-7dc8a65f4a0cda33.js": { contentType: "application/javascript", content: file35, module: null, isStatic: false },
+  "/.next/static/chunks/main-a5af4d1b6edd23b1.js": { contentType: "application/javascript", content: file36, module: null, isStatic: false },
+  "/.next/static/chunks/pages/_app-e64cee366c168257.js": { contentType: "application/javascript", content: file37, module: null, isStatic: false },
+  "/.next/static/chunks/pages/_error-a4ba2246ff8fb532.js": { contentType: "application/javascript", content: file38, module: null, isStatic: false },
+  "/.next/static/chunks/pages/index-cffb9a60bb2b6465.js": { contentType: "application/javascript", content: file39, module: null, isStatic: false },
+  "/.next/static/chunks/polyfills-c67a75d1b6f99dc8.js": { contentType: "application/javascript", content: file40, module: null, isStatic: false },
+  "/.next/static/chunks/webpack-69bfa6990bb9e155.js": { contentType: "application/javascript", content: file41, module: null, isStatic: false },
+  "/.next/static/css/27d177a30947857b.css": { contentType: "text/css", content: file42, module: null, isStatic: false },
+  "/.next/static/css/adf6895ec356eb64.css": { contentType: "text/css", content: file43, module: null, isStatic: false },
+  "/.next/trace": { contentType: undefined, content: Buffer.from(file44, "base64"), module: null, isStatic: false },
+  "/README.md": { contentType: undefined, content: Buffer.from(file45, "base64"), module: null, isStatic: false },
+  "/next-env.d.ts": { contentType: undefined, content: Buffer.from(file46, "base64"), module: null, isStatic: false },
+  "/next.config.js": { contentType: "application/javascript", content: file47, module: fileModule47, isStatic: false },
+  "/package-lock.json": { contentType: "application/json", content: file48, module: null, isStatic: false },
+  "/package.json": { contentType: "application/json", content: file49, module: null, isStatic: false },
+  "/src/pages/_app.tsx": { contentType: undefined, content: Buffer.from(file50, "base64"), module: null, isStatic: false },
+  "/src/pages/api/hello.ts": { contentType: undefined, content: Buffer.from(file51, "base64"), module: null, isStatic: false },
+  "/src/pages/api/user/[name].ts": { contentType: undefined, content: Buffer.from(file52, "base64"), module: null, isStatic: false },
+  "/src/pages/index.tsx": { contentType: undefined, content: Buffer.from(file53, "base64"), module: null, isStatic: false },
+  "/src/public/favicon.ico": { contentType: "image/vnd.microsoft.icon", content: Buffer.from(file54, "base64"), module: null, isStatic: false },
+  "/src/public/vercel.svg": { contentType: "image/svg+xml", content: file55, module: null, isStatic: false },
+  "/src/styles/Home.module.css": { contentType: "text/css", content: file56, module: null, isStatic: false },
+  "/src/styles/globals.css": { contentType: "text/css", content: file57, module: null, isStatic: false },
+  "/tsconfig.json": { contentType: "application/json", content: file58, module: null, isStatic: false },
+};
+
+export const spaFile = false;
+
+export const notFoundPageFile = false;
+
+export const autoIndex = [];
+
+export const autoExt = [];
